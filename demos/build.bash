3643fadfed666cf5c7d3031fa4ef1da98fab3f6e
bob <robertwalks@gmail.com>
2020-07-08 09:38:47 -0400

    init: vue

---
+++ demos/build.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# build vue app and push to github pages
+
+echo "building..."
+yarn build
+
+echo "copying..."
+T=$(date +'%Y%M%d%H%M')
+tmp_dir="/tmp/ghp_dist_"$T
+
+echo "copying dist/ to "$tmp_dir" ..."
+if [[ ! -d "./dist" ]];then
+  echo "!! err: no dist directory present; exiting..."
+  return
+fi
+cp -r ./dist $tmp_dir 
+
+echo "switching branches..."
+cd ..
+git checkout master
+
+echo "moving dist to repo ..."
+echo `cp -r $tmp_dir/* ./`
+return
+echo "and committing && pushing to master..."
+git add -A && git commit -am "publishing "$T && git push
+
+echo "returning to main"
+git checkout main

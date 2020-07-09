aaa0049d6b9373e9a49a4b3bd630de168b40e12f
bob <robertwalks@gmail.com>
2020-07-09 10:19:32 -0400

    edit: query parser working, imports from qbuilder

--- demos/src/components/search/SchematicQuery.vue
+++ demos/src/components/search/SchematicQuery.vue
@@ -1,12 +1,7 @@
 <template>
   <div v-if="queryArr.length">
     <span v-for="(q, i) in queryArr" :key="i">
-      <span
-        @click="nextOp(i)"
-        class="op"
-        :style="{ backgroundColor: q.color }"
-        >{{ q.op }}</span
-      >
+      <span @click="nextOp(i)" class="op" :style="{ backgroundColor: q.color }">{{ q.op }}</span>
       <span v-if="bolds(q)" class="bold">{{ q.val[0] }}</span>
       <span class="text">{{ bolds(q) ? q.val.slice(1, -1) : q.val }}</span>
       <span v-if="bolds(q)" class="bold">{{ q.val.slice(-1) }}</span>
@@ -16,12 +11,18 @@
 
 <script>
 import { mapState } from 'vuex'
-
+import { parseQuery } from '@hp4k1h5/aqlquerybuilder.js'
 export default {
   computed: {
-    ...mapState({
-      query: state => state.search.query,
-    }),
+    ...mapState({}),
+    query: {
+      get: function() {
+        return this.$store.state.search.query
+      },
+      set: function(val) {
+        this.$store.state.search.query = val
+      },
+    },
 
     queryArr: {
       get: function() {
@@ -34,42 +35,17 @@ export default {
   },
 
   methods: {
-    parseQuery() {
-      if (!this.query || !this.query.length) this.queryArr = []
-      const queryRgx = /(["'(]).+?(\1|\))|[^"'()\s]+/g
-      let matches = this.query.match(queryRgx)
-      if (!matches) return {}
-      matches = matches.filter(m => /\S/.test(m))
-      matches = matches.map(m => {
-        const mm = this.queryArr.find(p => p.val === m)
-        if (mm) {
-          return mm
-        } else if (['"', "'"].includes(m[0])) {
-          return {
-            type: 'phr',
-            val: m,
-            op: '+',
-            color: this.colorMap('+'),
-          }
-        } else if (m[0] == '(') {
-          return {
-            type: 'prox',
-            val: m,
-            dist: 5,
-            op: '+',
-            color: this.colorMap('+'),
-          }
-        } else {
-          return { type: 'ana', val: m, op: '?', color: this.colorMap('?') }
-        }
-      })
-      this.queryArr = matches
-    },
-
     nextOp(i) {
       const ops = ['?', '+', '-']
       const opI = ops.findIndex(o => o === this.queryArr[i].op)
       const next = ops[(opI + 1) % ops.length]
+      const termI = this.query.indexOf(this.queryArr[i].val)
+      if (!termI || this.query[termI - 1] == ' ') {
+        this.query = this.query.slice(0, termI) + next + this.query.slice(termI)
+      } else {
+        this.query =
+          this.query.slice(0, termI - 1) + next + this.query.slice(termI)
+      }
       this.queryArr[i].op = next
       this.queryArr[i].color = this.colorMap(next)
     },
@@ -90,7 +66,10 @@ export default {
 
   watch: {
     query() {
-      this.parseQuery()
+      this.queryArr = parseQuery(this.query).map(t => ({
+        ...t,
+        color: this.colorMap(t.op),
+      }))
     },
   },
 }
